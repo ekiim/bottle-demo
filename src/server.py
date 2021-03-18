c7a0294276c9c7b90d6e9a941037a8e60b5d23c6
Miguel Alejandro Salgado Zapien <mikesalzap@gmail.com>
2021-03-17 22:29:56 -0700

    Archivo programa servidor de datos.

---
+++ src/server.py
@@ -0,0 +1,48 @@
+import json
+import bottle
+
+app = bottle.Bottle()
+
+@app.route("/", method=["GET"])
+def index():
+    return {"status": "OK"}
+
+@app.route("/contact", method=["POST"])
+def get_contacts():
+    formdata = bottle.request.forms
+    print(formdata)
+    return {"status": "OK"}
+
+@app.route("/name/<name>", method=["GET"])
+def get_name(name="Mike"):
+    return {
+        "status": "OK",
+        "name": name
+    }
+
+@app.route("/error/zero", method=["GET"])
+def get_error(name="Mike"):
+    value = 1 / 0
+    return {"division by zero": value}
+
+@app.error(404)
+def error_404(error):
+    bottle.response.status = 404
+    bottle.response.content_type = "application/json"
+    return json.dumps({"status": "Ups"})
+
+@app.error(405)
+def error_405(error):
+    bottle.response.status = 405
+    bottle.response.content_type = "application/json"
+    return json.dumps({"status": "Accediste mal, esto no soporta el verbo http que quieres usar."})
+
+@app.error(500)
+def error_500(error):
+    bottle.response.status = 500
+    bottle.response.content_type = "application/json"
+    return json.dumps({"status": "Super Ups"})
+
+if __name__ == '__main__':
+    print("Iniciando servidor")
+    app.run(host="0.0.0.0", port=9999, debug=True)
